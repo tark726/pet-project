e87f009c7ef2566ba435381e040fd7290e773e12
tark726 <73743887+tark726@users.noreply.github.com>
2021-02-04 18:05:25 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -2,7 +2,7 @@ class Food {
     constructor () {
       
         this.foodStock = 0;
-        this.image = loadImage("images/Milk.png");
+        this.image = loadImage("Milk.png");
         this.lastFed;
     }
 
